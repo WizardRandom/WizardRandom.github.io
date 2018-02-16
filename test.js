c8ab9be90c9a5dccca9deee04865728e037ef1c9
WizardRandom <NCraw2099@aol.com>
2018-02-16 13:41:50 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ try {
   var gsheet = require('gsheet-web');
 }
 
-gsheet('1KPJr7wBEzicgrqukhEZBdGCqNQQB-ldrRrS0lxmq_qo', (data)=>{
+gsheet('1LEKra-mjOrJq7tG2JKU1pYclpDWKxTMnbgCpGZHBemY', (data)=>{
   console.log('Try callback ', data.length);  // array of objects
 });
 
-gsheet('1KPJr7wBEzicgrqukhEZBdGCqNQQB-ldrRrS0lxmq_qo').then((data)=>{
+gsheet('1LEKra-mjOrJq7tG2JKU1pYclpDWKxTMnbgCpGZHBemY').then((data)=>{
   console.log('Try promise ', data.length);
 });
